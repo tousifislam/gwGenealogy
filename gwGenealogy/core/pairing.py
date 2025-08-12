3ea68bdd803e0b489f006fdd765c7fbc7782f8a4
tousifislam <tousifislam24@gmail.com>
2025-08-12 14:43:21 -0400

    add module for pairing two bhs

---
+++ gwGenealogy/core/pairing.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+#==============================================================================
+#
+#    FILE: pairing.py
+#
+#    AUTHOR: Tousif Islam
+#    CREATED: 08-11-2025
+#    LAST MODIFIED: 
+#    REVISION: ---
+#==============================================================================
+__author__ = "Tousif Islam"
+
+import numpy as np
+import matplotlib.pyplot as plt
