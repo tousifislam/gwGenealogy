a07d9676d865a3601e0b315ef6e1a913f5dac615
tousifislam <tousifislam24@gmail.com>
2025-11-16 01:19:58 -0800

    update vescape

--- gwGenealogy/__init__.py
+++ gwGenealogy/__init__.py
@@ -1,3 +1,4 @@
 from . import core
 from . import remnants
 from . import vescape
+from . import detections
--- gwGenealogy/vescape/__init__.py
+++ gwGenealogy/vescape/__init__.py
@@ -1,2 +1,3 @@
 from .globular_clusters_vesc import *
 from .galaxies_vesc import *
+from .analytical_vesc import *
